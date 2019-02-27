119bb04b7c72ac3f18f9a100207a2cdecaa02f39
Ralph Croft <RalphRCroft@googlemail.com>
2019-02-27 14:45:58 +0000

    Module 3

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+
+        public string Description
+        {
+             get; set; 
+        }
+    }
+}
